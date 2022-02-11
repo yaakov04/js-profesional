aa752d2609a08e4be085465202dc72402700239e
Yaakov <dbrionesmtz@yahoo.com>
2022-02-11 11:54:44 -0600

    Tipos basicos en typescript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,64 @@
-console.log('hyat')
+// Boolean
+let muted: boolean = true;
 
-function sum(a: number, b: number){
-    return a+b;
+muted = false;
+
+// no admite muted = 'true';
+
+
+// Numeros
+
+let num: number = 42;
+let denom: number = 6;
+// no admite denom = '6';
+
+let result = num / denom;
+
+
+// String
+
+let nombre: string = 'Diego';
+let saludo = `Me llamod ${nombre}`;
+
+
+// Arrays
+
+let people: string[];
+
+people = ['Isa', 'Nicol', 'Raul'];
+
+people.push('Juan');
+//no admite people.push(2);
+
+let peopleAndNumbers: Array<string | number> = [];
+
+peopleAndNumbers.push('Ricardo');
+peopleAndNumbers.push(40);
+
+
+
+// Enum
+
+enum colorEnum {
+    Red = 'Rojo',
+    Verde = 'Green',
+    Azul = 'Azul'
 }
 
-const suma = sum(2,3);
+let colorFavorito = colorEnum.Azul;
+
+console.log(colorFavorito);
+
+
+// Any
+
+let comodin: any = 'Joker';
+comodin = {type: 'F'};
+
+
+// Object
+let obj: object = {color: 'Rojo'};
+
+//no admite obj = 2;
+
+obj = {comida: 'azul', bool:true};
